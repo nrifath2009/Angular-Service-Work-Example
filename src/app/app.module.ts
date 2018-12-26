392bbaa4f3bd5546c16e7f38f6bc51f8944d7608
Md. Nahidur Rahman <nrifath2009@gmail.com>
2018-12-27 01:08:38 +0600

    Implement router feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import{RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SandboxComponent } from './components/sandbox.component';
 import {DataService} from './services/data.service';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes:Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'users',component:SandboxComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    SandboxComponent
+    SandboxComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)    
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
